41b386464fad87dd93f7f3e472eac4c21eccc2eb
ctrevai <ctrevai@gmail.com>
2024-05-22 09:01:45 +0000

    add AnalyseInvoice Class

--- stfrontend/src/app.py
+++ stfrontend/src/app.py
@@ -6,21 +6,7 @@ import time
 import random
 import string
 
-
-st.title("Invoice to Pay AI Agent")
-st.subheader("Overview/Introduction")
-st.info('''
-        This is a minimum viable product demo of Dorje's AI agent feature. With Dorje AI, users can create custom AI agents to assist with tasks like invoice processing, sales, and workflow optimization. \n
-        In this capability demo, we focus on the invoice processing use case, showcasing our agent's ability to interpret uploaded PDF invoices and extract key information in bulk, such as PO record matching, invoice amounts, and supplier details. \n
-        Try how our AI agent works through a simple chat prompt. Upload an invoice and ask the agent questions about it. \n
-        For the purpose of this demo, the approval qualification for invoices is set at $1000. Invoices below this amount will be auto-processed by the agent.
-        ''')
 dorje_logo = "./Dorje logo.svg"
-st.sidebar.image(dorje_logo, width=200, output_format='PNG')
-st.sidebar.subheader("Dorje AI demo")
-st.sidebar.info("This demo is powered by Dorje AI")
-
-region = 'us-east-1'
 
 agentId = "XSSIC35SEK"
 agentAliasId = "LLGCKF4OII"
@@ -29,7 +15,7 @@ knowledgeBaseId = "65RMT6TTOW"
 knowledge_base_s3_bucket = "invoice-to-pay-kb"
 
 # export AWS_PROFILE='your_profile' for local test
-session = boto3.Session(region_name=region)
+session = boto3.Session()
 bedrock_client = session.client('bedrock')
 s3_client = session.client('s3')
 agent_client = session.client('bedrock-agent')
@@ -59,17 +45,6 @@ def session_generator():
     print("Session ID: " + str(pattern))
 
     return pattern
-
-
-def delete_previous_invoice():
-    print("deleting previous invoice")
-    objects_to_delete = s3_client.list_objects_v2(
-        Bucket=knowledge_base_s3_bucket)
-    if 'Contents' in objects_to_delete:
-        for object in objects_to_delete['Contents']:
-            print("file to be deleted :" + object['Key'])
-            s3_client.delete_object(
-                Bucket=knowledge_base_s3_bucket, Key=object['Key'])
 
 
 def process_response(response):
@@ -146,36 +121,6 @@ def bedrock(query, sessionId):
             return agent_response
 
 
-def update_knowledge_base(file_content, bucket_name, s3_file_name):
-    print("Syncing Invoice to Knowledge Base")
-
-    try:
-        file_obj = io.BytesIO(file_content)
-
-        s3_client.upload_fileobj(
-            file_obj,
-            bucket_name,
-            s3_file_name,
-        )
-    except Exception as e:
-        st.error(f"Error uploading file to S3: {e}")
-        return
-
-    # Ingest the file to the knowledge base
-    description = "Ingestion and update knowledge base data source"
-    print(description)
-    try:
-        response = agent_client.start_ingestion_job(
-            dataSourceId=dataSourceId,
-            knowledgeBaseId=knowledgeBaseId,
-            description=description
-        )
-    except Exception as e:
-        st.error(f"Error starting ingestion job: {e}")
-    finally:
-        file_obj.close()
-
-
 def check_ingestion_job_status():
 
     job_status = ""
@@ -189,7 +134,7 @@ def check_ingestion_job_status():
             if response['ResponseMetadata']['HTTPStatusCode'] == 200:
                 job_status = response["ingestionJobSummaries"][0]["status"]
                 print(f"Ingestion Job Status: {job_status}")
-                st.write(f"Ingestion Job Status: {job_status}")
+                # st.write(f"Ingestion Job Status: {job_status}")
             else:
                 st.write(
                     f"Error: {response['ResponseMetadata']['HTTPStatusCode']}")
@@ -199,93 +144,157 @@ def check_ingestion_job_status():
 
         except Exception as e:
             st.write(f"An error occurred: {e}")
-
-        time.sleep(10)  # Poll every 4 seconds (adjust as needed)
+        with st.spinner('Wait for Ingestion Job to Complete'):
+            time.sleep(10)  # Poll every 4 seconds (adjust as needed)
 
 
 def show_pdf(uploaded_file):
     # Display PDF preview
+    # print("show pdf")
+    pdf_file = uploaded_file.read()
     st.subheader("PDF Preview")
     pdf_display = f'<iframe src="data:application/pdf;base64,{base64.b64encode(
-        uploaded_file.read()).decode("utf-8")}" width="100%" height="500"></iframe>'
+        pdf_file).decode("utf-8")}" width="100%" height="500"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
 
 
-def process_uploaded_file(uploaded_file):
-    file_name = uploaded_file.name.lower()
-    file_extension = file_name.split('.')[-1]
+class AnalyseInvoice:
 
-    file_contents = None
+    def update_knowledge_base(self, file_content):
+        print("Syncing Invoice to Knowledge Base")
+        bucket_name = knowledge_base_s3_bucket
+        s3_file_name = "agent/knowledge-base-assets/invoices/" + \
+            "invoice_" + st.session_state.uploaded_file.name
 
-    if file_extension == 'pdf':
-        show_pdf(uploaded_file)
-        file_contents = uploaded_file.getvalue()
+        try:
+            file_obj = io.BytesIO(file_content)
 
-    else:
-        # Unsupported file type
-        st.error("Unsupported file type. Please upload a PDF file.")
+            s3_client.upload_fileobj(
+                file_obj,
+                bucket_name,
+                s3_file_name,
+            )
+        except Exception as e:
+            st.error(f"Error uploading file to S3: {e}")
+            return
 
-    return file_contents
+        # Ingest the file to the knowledge base
+        description = "Ingestion and update knowledge base data source"
+        print(description)
+        try:
+            response = agent_client.start_ingestion_job(
+                dataSourceId=dataSourceId,
+                knowledgeBaseId=knowledgeBaseId,
+                description=description
+            )
+        except Exception as e:
+            st.error(f"Error starting ingestion job: {e}")
+        finally:
+            file_obj.close()
 
+    def delete_previous_invoice(self):
+        print("deleting previous invoice")
+        objects_to_delete = s3_client.list_objects_v2(
+            Bucket=knowledge_base_s3_bucket)
+        if 'Contents' in objects_to_delete:
+            for object in objects_to_delete['Contents']:
+                print("file to be deleted :" + object['Key'])
+                s3_client.delete_object(
+                    Bucket=knowledge_base_s3_bucket, Key=object['Key'])
 
-def main():
-    if not "valid_inputs_receieved" in st.session_state:
-        st.session_state["valid_inputs_receieved"] = False
+    def process_uploaded_file(self):
+        file_name = st.session_state.uploaded_file.name.lower()
+        file_extension = file_name.split('.')[-1]
 
-    # Invoice upload
-    st.subheader("Step1: Upload Invoice")
-    uploaded_file = st.file_uploader("Choose a file", type=["pdf"])
-    if uploaded_file is not None:
-        # print("uploaded invoice :" + str(uploaded_file))
-        # print("current invoice :" + str(st.session_state.get("uploaded_file")))
-        if uploaded_file != st.session_state.get("uploaded_file"):
-            delete_previous_invoice()
-            # print("session state uploaded_file: " + str(st.session_state.get("uploaded_file")))
-            st.session_state["uploaded_file"] = uploaded_file
-            # print("session state uploaded_file: " + str(st.session_state.get("uploaded_file")))
-            print("Uploading new invoice")
-            # st.session_state["session_id"] = session_generator()
-            file_name = "agent/knowledge-base-assets/invoices/" + \
-                "invoice_" + uploaded_file.name
-            file_contents = process_uploaded_file(uploaded_file)
-            update_knowledge_base(
-                file_contents, knowledge_base_s3_bucket, file_name)
-            check_ingestion_job_status()
+        file_contents = None
+
+        if file_extension == 'pdf':
+            file_contents = st.session_state.uploaded_file.getvalue()
+            return file_contents
         else:
-            show_pdf(uploaded_file)
+            st.error("Unsupported file type. Please upload a PDF file.")
 
-    # Prompt input
-    st.subheader("Step 2: Ask anything about your invoice")
-    query = st.text_input("Enter your query", value="",
-                          placeholder="Ask question about invoice", label_visibility="visible")
-    agent_response = None
-    if st.session_state.get("previous_query") != query and query != "":
+    def ask_anything(self):
+
+        # Prompt input
+        st.subheader("Step 2: Ask anything about your invoice")
+        query = st.text_input("Enter your query", value="",
+                              placeholder="Ask question about invoice", label_visibility="visible")
+        agent_response = None
+        if st.session_state.get("previous_query") != query and query != "":
+            sessionId = st.session_state["session_id"]
+            agent_response = bedrock(query, sessionId)
+            st.session_state["previous_query"] = query
+
+        if st.button("Submit"):
+            sessionId = st.session_state["session_id"]
+            agent_response = bedrock(query, sessionId)
+            st.session_state["previous_query"] = query
+
+        if st.button("Check Invoice Qualification"):
+            sessionId = st.session_state["session_id"]
+            agent_response = bedrock(approval_query, sessionId)
+            st.session_state["previous_query"] = query
+
+        if agent_response is not None:
+            printable = agent_response.replace("$", "\$")
+            st.write(f"Agent's Response:")
+            st.write(f"{printable}")
+
+    def upload_invoice(self):
+        st.subheader("Step1: Upload Invoice")
+        uploaded_file = st.file_uploader(
+            "Choose a file", type=["pdf"])
+        if uploaded_file is not None:
+
+            if uploaded_file != st.session_state.uploaded_file:
+                st.session_state.uploaded_file = uploaded_file
+                # st.write("Uploaded file:", uploaded_file.name)
+
+                print("Uploading new invoice")
+                self.delete_previous_invoice()
+                file_contents = self.process_uploaded_file()
+                self.update_knowledge_base(file_contents)
+                check_ingestion_job_status()
+                show_pdf(uploaded_file)
+            else:
+                show_pdf(uploaded_file)
+
+    def sidebar(self):
+
+        st.sidebar.image(dorje_logo, width=200, output_format='PNG')
+        st.sidebar.subheader("Dorje AI demo")
+        st.sidebar.info("This demo is powered by Dorje AI")
+
+    def render(self):
+        st.set_page_config(page_title="Invoice to Pay AI Agent",
+                           page_icon=dorje_logo, layout="wide")
+
+        st.title("Invoice to Pay AI Agent")
+        st.subheader("Overview/Introduction")
+        st.info('''
+            This is a minimum viable product demo of Dorje's AI agent feature. With Dorje AI, users can create custom AI agents to assist with tasks like invoice processing, sales, and workflow optimization. \n
+            In this capability demo, we focus on the invoice processing use case, showcasing our agent's ability to interpret uploaded PDF invoices and extract key information in bulk, such as PO record matching, invoice amounts, and supplier details. \n
+            Try how our AI agent works through a simple chat prompt. Upload an invoice and ask the agent questions about it. \n
+            For the purpose of this demo, the approval qualification for invoices is set at $1000. Invoices below this amount will be auto-processed by the agent.
+            ''')
+
         if "session_id" not in st.session_state:
             st.session_state["session_id"] = session_generator()
 
-        sessionId = st.session_state["session_id"]
-        agent_response = bedrock(query, sessionId)
-        st.session_state["previous_query"] = query
+        if "previous_query" not in st.session_state:
+            st.session_state["previous_query"] = ""
 
-    if st.button("Submit"):
-        if "session_id" not in st.session_state:
-            st.session_state["session_id"] = session_generator()
-        sessionId = st.session_state["session_id"]
-        agent_response = bedrock(query, sessionId)
-        st.session_state["previous_query"] = query
+        if "uploaded_file" not in st.session_state:
+            st.session_state["uploaded_file"] = None
 
-    if st.button("Check Invoice Qualification"):
-        if "session_id" not in st.session_state:
-            st.session_state["session_id"] = session_generator()
-        sessionId = st.session_state["session_id"]
-        agent_response = bedrock(approval_query, sessionId)
-        st.session_state["previous_query"] = query
+        if "analyse_complete" not in st.session_state:
+            st.session_state["analyse_complete"] = False
 
-    if agent_response is not None:
-        printable = agent_response.replace("$", "\$")
-        st.write(f"Agent's Response:")
-        st.write(f"{printable}")
+        self.sidebar()
+        self.upload_invoice()
+        self.ask_anything()
 
 
 if __name__ == "__main__":
-    main()
+    AnalyseInvoice().render()
